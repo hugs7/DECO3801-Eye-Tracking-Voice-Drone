fc1512234065cc5620ca804bebde3b0a8eddbfa0
Jas2310 <jasmine.hong@uqconnect.edu.au>
2024-08-02 10:23:44 +1000

    Basic gaze stuff

--- eye_tracking/main.py
+++ eye_tracking/main.py
@@ -4,4 +4,19 @@ Eye tracking driver
 """
 
 from eyeGestures.utils import VideoCapture
-from eyeGestures.eye_tracker import EyeTracker
+from eyeGestures.eyegestures import EyeGestures_v2
+
+# Initialize gesture engine and video capture
+gestures = EyeGestures_v2()
+cap = VideoCapture(0)  
+calibrate = True
+screen_width = 500
+screen_height= 500
+
+# Process each frame
+while True:
+  ret, frame = cap.read()
+  event, cevent = gestures.step(frame, calibrate, screen_width, screen_height)
+  
+  cursor_x, cursor_y = event.point[0], event.point[1]
+  # calibration_radius: radius for data collection during calibration
