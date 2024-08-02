d297e338fdcbbb57751e6d9e40ecfe8f86f5821e
Hugo Burton <burto732@gmail.com>
2024-08-02 10:58:20 +1000

    Add function for normalising landmark

--- eye_tracking/screen_res.py
+++ eye_tracking/screen_res.py
@@ -7,10 +7,21 @@ import cv2
 import time
 import mediapipe as mp
 
+# Initialize variables
 run = True
+print_interval = 5  # Time interval in seconds
+last_print_time = time.time()
 
+# Initialize video capture and face mesh detector
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh()
+
+
+def normalise_landmark(landmark, frame_w, frame_h):
+    x = int(landmark.x * frame_w)
+    y = int(landmark.y * frame_h)
+    return x, y
+
 
 while True:
     _, frame = cam.read()
@@ -22,13 +33,20 @@ while True:
     frame_h, frame_w, _ = frame.shape
 
     if points:
-        # print(points)
         landmarks = points[0].landmark
-        for landmark in landmarks:
-            x = int(landmark.x * frame_w)
-            y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
-            print(x, y)
+        current_time = time.time()
+        if current_time - last_print_time > print_interval:
+            last_print_time = current_time
+            print(landmarks)
+            for landmark in landmarks:
+                x, y = normalise_landmark(landmark, frame_w, frame_h)
+                cv2.circle(frame, (x, y), 3, (0, 255, 0))
+                # print(x, y)
 
     cv2.imshow("Eye Tracking", frame)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Release the resources
+cam.release()
+cv2.destroyAllWindows()
