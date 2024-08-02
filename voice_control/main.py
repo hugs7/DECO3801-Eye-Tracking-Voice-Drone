a0f8b1322814a4d265748981edde4cfd0d944070
Hugo Burton <burto732@gmail.com>
2024-08-02 10:10:25 +1000

    Working audio detection

--- voice_control/main.py
+++ voice_control/main.py
@@ -1,12 +1,26 @@
 import speech_recognition as sr
+import sounddevice as sd
+import numpy as np
 
 recognizer = sr.Recognizer()
 
+
+def print_volume(indata, frames, time, status):
+    volume_norm = np.linalg.norm(indata) * 10
+    print(f"Microphone Volume: {volume_norm:.2f}")
+
+
 def capture_voice_input():
+    with sd.InputStream(callback=print_volume):
+        sd.sleep(10000)
+
+    return
     with sr.Microphone() as source:
-        print("Listening...")
+        print(f"Listening on source", source.list_microphone_names(), "\n\n", source.list_working_microphones())
+
         audio = recognizer.listen(source)
     return audio
+
 
 def convert_voice_to_text(audio):
     try:
@@ -20,6 +34,7 @@ def convert_voice_to_text(audio):
         print("Error; {0}".format(e))
     return text
 
+
 def process_voice_command(text):
     if "left" in text.lower():
         print("Left")
@@ -30,12 +45,14 @@ def process_voice_command(text):
         print("Command Not understood. Try again.")
     return False
 
+
 def main():
     end_program = False
     while not end_program:
         audio = capture_voice_input()
         text = convert_voice_to_text(audio)
         end_program = process_voice_command(text)
+
 
 if __name__ == "__main__":
     main()
