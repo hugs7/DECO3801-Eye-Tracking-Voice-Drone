8d312b23f99259bd138efdb98730e7652ab18720
Hugo Burton <burto732@gmail.com>
2024-08-02 23:06:12 +1000

    Add colour mapping

---
+++ eye_tracking/colours.py
@@ -0,0 +1,31 @@
+"""
+Defines some colours
+"""
+
+from typing import Tuple
+
+
+class ColourMap:
+    red: Tuple[int, int, int] = (0, 0, 255)
+    green: Tuple[int, int, int] = (0, 255, 0)
+    blue: Tuple[int, int, int] = (255, 0, 0)
+    yellow: Tuple[int, int, int] = (226, 235, 52)
+    line: Tuple[int, int, int] = (153, 235, 52)
+    light_green: Tuple[int, int, int] = (104, 235, 5)
+    spring_green: Tuple[int, int, int] = (52, 235, 76)
+    forest_green: Tuple[int, int, int] = (52, 235, 104)
+    torquoise: Tuple[int, int, int] = (52, 235, 174)
+    cyan: Tuple[int, int, int] = (52, 229, 235)
+    sky_blue: Tuple[int, int, int] = (52, 162, 235)
+    cornflower_blue: Tuple[int, int, int] = (52, 116, 235)
+    royal_blue: Tuple[int, int, int] = (52, 52, 235)
+    navy_blue: Tuple[int, int, int] = (116, 52, 235)
+    purple: Tuple[int, int, int] = (162, 52, 235)
+    magenta: Tuple[int, int, int] = (235, 52, 229)
+    pink: Tuple[int, int, int] = (235, 52, 162)
+    coral: Tuple[int, int, int] = (235, 52, 116)
+    orange: Tuple[int, int, int] = (235, 104, 52)
+    brown: Tuple[int, int, int] = (235, 162, 52)
+    gold: Tuple[int, int, int] = (235, 229, 52)
+    white: Tuple[int, int, int] = (255, 255, 255)
+    black: Tuple[int, int, int] = (0, 0, 0)
