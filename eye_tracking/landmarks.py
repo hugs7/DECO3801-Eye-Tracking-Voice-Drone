62261d20fe5a1aae1af591eff66eae51064e2202
Hugo Burton <burto732@gmail.com>
2024-08-02 22:00:08 +1000

    Add landmarks

--- eye_tracking/landmarks.py
+++ eye_tracking/landmarks.py
@@ -20,3 +20,12 @@ eye_landmarks = {
 }
 
 face_landmarks = [i for i in range(0, 478)]
+
+landmark_mapping = {
+    "eyebrow": {
+        "left": [70, 63, 105, 66, 65, 52, 53],
+        "right": [336, 296, 334, 293, 300, 276, 283, 282, 295],
+    },
+    "lips": [39, 37, 267],
+    "moustache": [],
+}
