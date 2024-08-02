572ec5036f05d80cd2f5ec7043b39cf3b53e1bf5
Hugo Burton <burto732@gmail.com>
2024-08-02 10:50:18 +1000

    Add facial recognition

--- eye_tracking/screen_res.py
+++ eye_tracking/screen_res.py
@@ -4,13 +4,30 @@ Screen res
 """
 
 import cv2
+import time
+import mediapipe as mp
 
-cam = cv2.VideoCapture(0)
 run = True
 
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh()
 
-while run:
+while True:
     _, frame = cam.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    points = output.multi_face_landmarks
+
+    frame_h, frame_w, _ = frame.shape
+
+    if points:
+        # print(points)
+        landmarks = points[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            print(x, y)
 
     cv2.imshow("Eye Tracking", frame)
     cv2.waitKey(1)
