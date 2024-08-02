e99301555e78ab65d3ffb8eae86c8ab52ab867f5
Hugo Burton <burto732@gmail.com>
2024-08-02 14:47:23 +1000

    Add landmark mapping

---
+++ eye_tracking/landmarks.py
@@ -0,0 +1,22 @@
+"""
+Defines the mapping for landmarks
+"""
+
+total_landmarks = 478
+
+eye_landmarks = {
+    "left": {
+        "top": 469,
+        "bottom": 470,
+        "left": 471,
+        "right": 472,
+    },
+    "right": {
+        "top": 473,
+        "bottom": 474,
+        "left": 475,
+        "right": 476,
+    },
+}
+
+face_landmarks = [i for i in range(0, 468)]
