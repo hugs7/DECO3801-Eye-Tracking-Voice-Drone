073c0dd865010b7ad07bc2881865ae5f5db49c1b
Hugo Burton <burto732@gmail.com>
2024-08-02 10:40:58 +1000

    Add video capture

--- eye_tracking/screen_res.py
+++ eye_tracking/screen_res.py
@@ -3,20 +3,14 @@ Screen res
 02/08/2024
 """
 
-from eyeGestures.utils import VideoCapture
-from eyeGestures.eyegestures import EyeGestures_v2
+import cv2
 
-# Initialize gesture engine and video capture
-gestures = EyeGestures_v2()
-cap = VideoCapture(0)
-calibrate = True
-screen_width = 500
-screen_height = 500
+cam = cv2.VideoCapture(0)
+run = True
 
-# Process each frame
-while True:
-    ret, frame = cap.read()
-    event, cevent = gestures.step(frame, calibrate, screen_width, screen_height, context="my_context")
 
-    cursor_x, cursor_y = event.point[0], event.point[1]
-    # calibration_radius: radius for data collection during calibration
+while run:
+    _, frame = cam.read()
+
+    cv2.imshow("Eye Tracking", frame)
+    cv2.waitKey(1)
