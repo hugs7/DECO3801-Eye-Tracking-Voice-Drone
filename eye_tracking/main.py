1180a17f3f5312ff09404d58d2b8087c7807b039
Hugo Burton <burto732@gmail.com>
2024-08-03 13:01:38 +1000

    Add main init

--- eye_tracking/main.py
+++ eye_tracking/main.py
@@ -3,11 +3,35 @@ Eye tracking driver
 02/08/2024
 """
 
+import cv2
+from mediapipe.python.solutions.face_mesh import FaceMesh
+
 import init
+import loop
+
+# Initialize video capture and face mesh detector
 
 
 def main():
-    landmark_mapping = init.eye_tracking_init()
+    landmark_mapping = init.landmark_mapping_init()
+
+    # Initialize variables
+    run = True
+    calibrated = False
+    window_width = 1280  # Replace with your desired width
+    window_height = 900
+
+    init.window_init(window_width, window_height)
+
+    cam = init.camera_init()
+    face_mesh = init.face_mesh_init()
+
+    while run:
+        run, calibrated = loop.main_loop(calibrated, cam, face_mesh, landmark_mapping)
+
+    # Release the resources
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
