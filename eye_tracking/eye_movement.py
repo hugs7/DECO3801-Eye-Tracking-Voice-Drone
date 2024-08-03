5657bfc0b7885c150c37b8dd9fc3b257f676d788
Hugo Burton <burto732@gmail.com>
2024-08-03 13:42:44 +1000

    Move track eye movement to own module

---
+++ eye_tracking/eye_movement.py
@@ -0,0 +1,35 @@
+"""
+Module to track eye movement
+"""
+
+import cv2
+
+import calibrate
+import landmarks
+
+
+def track_eye_movement(
+    frame, face_landmarks: landmarks.Landmarks, frame_w, frame_h, reference_positions: calibrate.ReferencePositions
+) -> None:
+    """
+    Tracks eye movement based on the face landmarks and draws a dot for where the user is looking
+    :param frame: The frame to draw on
+    :param face_landmarks: The face landmarks
+    :param frame_w: The width of the frame
+    :param frame_h: The height of the frame
+    :param reference_positions: The reference positions as per calibration
+    :return: None
+    """
+
+    sensitivity = 25
+    for eye in ["left", "right"]:
+        iris_x, iris_y = landmarks.normalise_landmark(face_landmarks[landmarks.eye_landmarks[eye]["top"]], frame_w, frame_h)
+        ref_x, ref_y = reference_positions[eye]["top"]
+
+        dx = iris_x - ref_x
+        dy = iris_y - ref_y
+
+        # Overlay a dot corresponding to the eye movement
+        overlay_x = frame_w // 2 + dx * sensitivity
+        overlay_y = frame_h // 2 + dy * sensitivity
+        cv2.circle(frame, (overlay_x, overlay_y), 5, (255, 0, 0), cv2.FILLED)
