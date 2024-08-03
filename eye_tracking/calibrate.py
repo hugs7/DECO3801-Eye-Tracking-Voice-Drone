76a05022b665393e2f156ff4a0836ca00093ddd0
Hugo Burton <burto732@gmail.com>
2024-08-03 22:07:44 +1000

    Add calibration loop
    Data not yet being collected

--- eye_tracking/calibrate.py
+++ eye_tracking/calibrate.py
@@ -2,22 +2,54 @@
 Module to help with eye tracking calibration
 """
 
-from typing import TypedDict
+from typing import Optional
 from enum import Enum
 import cv2
 
 import landmarks
+import constants
 import coordinate
 from custom_types.NormalisedLandmark import NormalisedLandmark
 from colours import ColourMap as CM
 
 
-class CalibrationSteps(Enum):
-    EYE_CENTRE = 1
-    EYE_TOP = 2
-    EYE_BOTTOM = 3
-    EYE_LEFT = 4
-    EYE_RIGHT = 5
+class CalibrationStep(Enum):
+    CENTER = "center"
+    TOP_LEFT = "top-left"
+    TOP_RIGHT = "top-right"
+    BOTTOM_LEFT = "bottom-left"
+    BOTTOM_RIGHT = "bottom-right"
+    MIDDLE_TOP = "middle-top"
+    MIDDLE_BOTTOM = "middle-bottom"
+    MIDDLE_LEFT = "middle-left"
+    MIDDLE_RIGHT = "middle-right"
+
+
+calibration_order = [
+    CalibrationStep.CENTER,
+    CalibrationStep.TOP_LEFT,
+    CalibrationStep.TOP_RIGHT,
+    CalibrationStep.BOTTOM_LEFT,
+    CalibrationStep.BOTTOM_RIGHT,
+    CalibrationStep.MIDDLE_TOP,
+    CalibrationStep.MIDDLE_BOTTOM,
+    CalibrationStep.MIDDLE_LEFT,
+    CalibrationStep.MIDDLE_RIGHT,
+]
+
+
+def get_next_calibration_step(step: CalibrationStep) -> Optional[CalibrationStep]:
+    """
+    Get the next calibration step
+    :param step: The current step
+    :return: The next step
+    """
+    current_index = calibration_order.index(step)
+    next_index = current_index + 1
+    if next_index >= len(calibration_order):
+        return None
+
+    return calibration_order[next_index]
 
 
 class EyeReferencePosition:
@@ -41,7 +73,11 @@ class CalibrationData:
         self.landmark_mapping = landmark_mapping
         self.frame_dim = frame_dim
 
-        self.step = CalibrationSteps.EYE_CENTRE
+        # Boolean for if user is looking straight ahead. User will need to
+        # keep their head straight during calibration
+        self.looking_straight: bool = True
+
+        self.step = calibration_order[0]
 
 
 def calibrate_init(
@@ -61,7 +97,7 @@ def calibrate_init(
 
     for eye in ["left", "right"]:
         for pos in ["top", "bottom", "left", "right", "centre"]:
-            eye_landmarks: landmarks.EyeLandmarks = eye_landmark_mapping[eye]
+            eye_landmarks: landmarks.EyeLandmarks = landmark_mapping.get_part_by_name(eye)
             eye_points: landmarks.EyePoints = eye_landmarks.points
             pos_id = eye_points.get_side(pos)
             eye_coord = face_landmarks[pos_id]
@@ -88,18 +124,46 @@ def perform_calibration(calibration_data: CalibrationData, frame: cv2.VideoCaptu
     padding_from_edge_y = int(padding_percentage * frame_dim.y)
 
     # Define dot positions programmatically
-    positions = [
-        (padding_from_edge_x, padding_from_edge_y),  # Top-left
-        (calibration_data.frame_dim.x - padding_from_edge_x, padding_from_edge_y),  # Top-right
-        (padding_from_edge_x, calibration_data.frame_dim.y - padding_from_edge_y),  # Bottom-left
-        (calibration_data.frame_dim.x - padding_from_edge_x, calibration_data.frame_dim.y - padding_from_edge_y),  # Bottom-right
-        (calibration_data.frame_dim.x // 2, calibration_data.frame_dim.y // 2),  # Center
-        (calibration_data.frame_dim.x // 2, padding_from_edge_y),  # Middle-top
-        (calibration_data.frame_dim.x // 2, calibration_data.frame_dim.y - padding_from_edge_y),  # Middle-bottom
-        (padding_from_edge_x, calibration_data.frame_dim.y // 2),  # Middle-left
-        (calibration_data.frame_dim.x - padding_from_edge_x, calibration_data.frame_dim.y // 2),  # Middle-right
-    ]
+    positions = {
+        CalibrationStep.CENTER: (calibration_data.frame_dim.x // 2, calibration_data.frame_dim.y // 2),
+        CalibrationStep.TOP_LEFT: (padding_from_edge_x, padding_from_edge_y),
+        CalibrationStep.TOP_RIGHT: (calibration_data.frame_dim.x - padding_from_edge_x, padding_from_edge_y),
+        CalibrationStep.BOTTOM_LEFT: (padding_from_edge_x, calibration_data.frame_dim.y - padding_from_edge_y),
+        CalibrationStep.BOTTOM_RIGHT: (
+            calibration_data.frame_dim.x - padding_from_edge_x,
+            calibration_data.frame_dim.y - padding_from_edge_y,
+        ),
+        CalibrationStep.MIDDLE_TOP: (calibration_data.frame_dim.x // 2, padding_from_edge_y),
+        CalibrationStep.MIDDLE_BOTTOM: (calibration_data.frame_dim.x // 2, calibration_data.frame_dim.y - padding_from_edge_y),
+        CalibrationStep.MIDDLE_LEFT: (padding_from_edge_x, calibration_data.frame_dim.y // 2),
+        CalibrationStep.MIDDLE_RIGHT: (calibration_data.frame_dim.x - padding_from_edge_x, calibration_data.frame_dim.y // 2),
+    }
 
     # Draw dots on the frame
-    for pos in positions:
-        cv2.circle(frame, pos, dot_radius, CM.red.get_colour(), -1, cv2.FILLED)
+    for pos_name, coord in positions.items():
+        if pos_name == calibration_data.step:
+            colour = CM.green
+        else:
+            colour = CM.grey
+
+        cv2.circle(frame, coord, dot_radius, colour.get_colour(), -1, cv2.FILLED)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key != 255:
+        print(f"Key: {key}")
+
+    if key == ord(constants.BACKSPACE_KEY):
+        # Continue with calibration
+        next_step = get_next_calibration_step(calibration_data.step)
+        if next_step is None:
+            print("Calibration complete")
+        else:
+            # Capture data from the current step
+
+            calibration_data.step = next_step
+            print(f"Moving to next calibration step: {calibration_data.step}")
+
+    elif key == ord("q") or key == ord(constants.ESCAPE_KEY):
+        print("Calibration cancelled")
+        calibration_data.step = CalibrationStep.CENTER
+        calibration_data.looking_straight = True
