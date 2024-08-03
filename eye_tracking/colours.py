16a60e695c2b44fbcb80c0ef1493d91740375351
Hugo Burton <burto732@gmail.com>
2024-08-03 11:22:21 +1000

    Add Colour class

--- eye_tracking/colours.py
+++ eye_tracking/colours.py
@@ -4,28 +4,49 @@ Defines some colours
 
 from typing import Tuple
 
+import math_utils
+
+MIN_COLOUR_VALUE = 0
+MAX_COLOUR_VALUE = 255
+
+
+class Colour:
+    def __init__(self, name: str, red: int, green: int, blue: int) -> None:
+        self.name = name
+
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+        # Check values
+        if not self._is_valid_colour_value(red):
+            raise ValueError(f"Invalid red value {red}")
+
+    def _is_valid_colour_value(self, value: int) -> bool:
+        return math_utils.in_range(value, MIN_COLOUR_VALUE, MAX_COLOUR_VALUE)
+
 
 class ColourMap:
-    red: Tuple[int, int, int] = (0, 0, 255)
-    green: Tuple[int, int, int] = (0, 255, 0)
-    blue: Tuple[int, int, int] = (255, 0, 0)
-    yellow: Tuple[int, int, int] = (226, 235, 52)
-    lime: Tuple[int, int, int] = (153, 235, 52)
-    light_green: Tuple[int, int, int] = (104, 235, 5)
-    spring_green: Tuple[int, int, int] = (52, 235, 76)
-    forest_green: Tuple[int, int, int] = (52, 235, 104)
-    torquoise: Tuple[int, int, int] = (52, 235, 174)
-    cyan: Tuple[int, int, int] = (52, 229, 235)
-    sky_blue: Tuple[int, int, int] = (52, 162, 235)
-    cornflower_blue: Tuple[int, int, int] = (52, 116, 235)
-    royal_blue: Tuple[int, int, int] = (52, 52, 235)
-    navy_blue: Tuple[int, int, int] = (116, 52, 235)
-    purple: Tuple[int, int, int] = (162, 52, 235)
-    magenta: Tuple[int, int, int] = (235, 52, 229)
-    pink: Tuple[int, int, int] = (235, 52, 162)
-    coral: Tuple[int, int, int] = (235, 52, 116)
-    orange: Tuple[int, int, int] = (235, 104, 52)
-    brown: Tuple[int, int, int] = (235, 162, 52)
-    gold: Tuple[int, int, int] = (235, 229, 52)
-    white: Tuple[int, int, int] = (255, 255, 255)
-    black: Tuple[int, int, int] = (0, 0, 0)
+    red = Colour("red", 0, 0, 255)
+    green = Colour("green", 0, 255, 0)
+    blue = Colour("blue", 255, 0, 0)
+    yellow = Colour("yellow", 226, 235, 52)
+    lime = Colour("lime", 153, 235, 52)
+    light_green = Colour("light_green", 104, 235, 5)
+    spring_green = Colour("spring_green", 52, 235, 76)
+    forest_green = Colour("forest_green", 52, 235, 104)
+    torquoise = Colour("torquoise", 52, 235, 174)
+    cyan = Colour("cyan", 52, 229, 235)
+    sky_blue = Colour("sky_blue", 52, 162, 235)
+    cornflower_blue = Colour("cornflower_blue", 52, 116, 235)
+    royal_blue = Colour("royal_blue", 52, 52, 235)
+    navy_blue = Colour("navy_blue", 116, 52, 235)
+    purple = Colour("purple", 162, 52, 235)
+    magenta = Colour("magenta", 235, 52, 229)
+    pink = Colour("pink", 235, 52, 162)
+    coral = Colour("coral", 235, 52, 116)
+    orange = Colour("orange", 235, 104, 52)
+    brown = Colour("brown", 235, 162, 52)
+    gold = Colour("gold", 235, 229, 52)
+    white = Colour("white", 255, 255, 255)
+    black = Colour("black", 0, 0, 0)
