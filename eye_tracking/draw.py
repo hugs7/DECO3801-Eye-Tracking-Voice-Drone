0becdfabee5b1af8c328733cd0cc5df510fc418b
Hugo Burton <burto732@gmail.com>
2024-08-03 14:35:04 +1000

    Fix draw runtime errors

--- eye_tracking/draw.py
+++ eye_tracking/draw.py
@@ -3,31 +3,34 @@ Module to draw on the screen
 """
 
 import cv2
+import cv2.typing as cv_t
 
 import landmarks
-from colours import ColourMap as CM
 import coordinate
 
 
-def draw_landmarks(frame, face_landmarks: landmarks.Landmarks, frame_dim: coordinate.Coordinate) -> None:
+def draw_landmarks(frame, face_landmarks, landmark_mapping: landmarks.Landmarks, frame_dim: coordinate.Coordinate) -> None:
     """
     Draws the landmarks on the frame
     :param frame: The frame to draw the landmarks on
+    :param face_landmarks: The landmarks processed by the face mesh
     :param landmark_mapping: The mapping of landmarks
-    :param frame_w: The width of the frame
-    :param frame_h: The height of the frame
+    :param frame_dim: The dimensions of the frame
     :return: None
     """
+
+    dot_radius = 3
+    font_scale = 0.3
+    thickness = 1
 
     for id, landmark in enumerate(face_landmarks):
         landmark_coord = landmarks.normalise_landmark(landmark, frame_dim)
 
-        point_class_label = face_landmarks.classify_point(id)
-        point_class = face_landmarks.landmark_mapping.get(point_class_label, None)
-        if point_class_label is None:
+        point_class = landmark_mapping.classify_point(id)
+        if point_class is None:
             raise ValueError(f"Point class not found for point {id}")
 
-        colour = point_class["colour"]
-
-        cv2.circle(frame, landmark_coord, 3, colour)
-        cv2.putText(frame, str(id), landmark_coord, cv2.FONT_HERSHEY_SIMPLEX, 0.3, CM.white, 1, cv2.LINE_AA)
+        centre = landmark_coord.to_point()
+        colour = point_class.colour.get_colour()
+        cv2.circle(frame, centre, dot_radius, colour)
+        cv2.putText(frame, str(id), centre, cv2.FONT_HERSHEY_SIMPLEX, font_scale, colour, thickness, cv2.LINE_AA)
