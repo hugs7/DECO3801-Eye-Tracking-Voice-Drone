694e13ae65c018027409a4d057c081196db4a206
Hugo Burton <burto732@gmail.com>
2024-08-03 12:37:02 +1000

    Add resolve path helper

--- eye_tracking/file_helper.py
+++ eye_tracking/file_helper.py
@@ -28,3 +28,23 @@ def load_json(json_path) -> dict:
 
     with open(json_path, "r") as file:
         return json.load(file)
+
+
+def resolve_path(relative_path: str, expect_exists: bool = True) -> str:
+    """
+    Takes a path relative to the entry point and returns the absolute path
+    :param relative_path: The relative path
+    :param expect_exists: True if the path is expected to exist (default), False otherwise
+    :return: The absolute path
+    """
+
+    # Get path of the entry point
+    entry_point = os.path.dirname(os.path.abspath(__file__))
+
+    # Join the entry point with the relative path
+    absolute_path = os.path.join(entry_point, relative_path)
+
+    if expect_exists and not check_path_exists(absolute_path):
+        raise FileNotFoundError(f"Path not found: {absolute_path}")
+
+    return absolute_path
