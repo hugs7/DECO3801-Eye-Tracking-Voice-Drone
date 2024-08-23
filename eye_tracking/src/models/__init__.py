654fdcc106429a4b31f6ad6142ce82818983e25a
Hugo Burton <burto732@gmail.com>
2024-08-23 13:11:47 +1000

    Fix resnet model loading

--- eye_tracking/src/models/__init__.py
+++ eye_tracking/src/models/__init__.py
@@ -3,11 +3,11 @@ import importlib
 import torch
 from omegaconf import DictConfig
 
+from .resnet_preact import Model
+
 
 def create_model(config: DictConfig) -> torch.nn.Module:
-    mode = config.mode
-    module = importlib.import_module(f"ptgaze.models.{mode.lower()}.{config.model.name}")
-    model = module.Model(config)
+    model = Model(config)
 
     device = torch.device(config.device)
     model.to(device)
