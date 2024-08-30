3b0d6f1f3988f4de3134bf560a345990d1e532c6
Hugo Burton <burto732@gmail.com>
2024-08-30 19:36:36 +1000

    (feat) wrapper to read camera feed
    (chore) constants file

---
+++ drone/constants.py
@@ -0,0 +1,13 @@
+"""
+Constnats file. Please define in UPPER_SNAKE_CASE
+"""
+
+# Drone Types
+
+MAVIC = "mavic"
+TELLO = "tello"
+DRONE_TYPES = [MAVIC, TELLO]
+
+
+# Rendered Window
+WINDOW_NAME = "Drone Capture"
--- drone/main.py
+++ drone/main.py
@@ -3,7 +3,9 @@ Main file for the drone
 """
 
 from typing import Union
-import pygame
+import cv2
+
+import constants as c
 
 import controller
 import mavic_drone
@@ -17,15 +19,45 @@ def loop(drone):
 
     controller.handle_input(drone)
 
+    img = read_camera_feed(drone)
+    # Show The Frame
+    render_drone_feed(img)
+
+    cv2.waitKey(1)
+
+
+def read_camera_feed(drone: Union[mavic_drone.Drone, tello_drone.Tello], drone_type: str):
+    """
+    Reads the camera feed from the drone
+    """
+
+    output_dim = (960, 720)
+
+    if drone_type == c.MAVIC:
+        img = mavic_drone.read_camera(drone)
+    elif drone_type == c.TELLO:
+        img = tello_drone.read_camera(drone)
+
+    img = cv2.resize(img, output_dim)
+    return img
+
+
+def render_drone_feed(img: cv2.typing.MatLike):
+    """
+    Renders the drone feed
+    """
+
+    cv2.imshow(c.WINDOW_NAME, img)
+
 
 def drone_connect(drone_type: str):
     """
     Connects to the drone
     """
 
-    if drone_type == "mavic":
+    if drone_type == c.MAVIC:
         drone = mavic_drone.mavic_connect()
-    elif drone_type == "tello":
+    elif drone_type == c.TELLO:
         drone = tello_drone.tello_connect()
 
     return drone
@@ -37,16 +69,16 @@ def init(drone_type):
     """
 
     drone = drone_connect(drone_type)
-    if drone_type == "mavic":
+    if drone_type == c.MAVIC:
         pass
-    elif drone_type == "tello":
+    elif drone_type == c.TELLO:
         tello_drone.init()
 
     return drone
 
 
 def main():
-    drone_type = "tello"  # "mavic"
+    drone_type = "tello"  # / c.MAVIC
 
     drone = init(drone_type)
 
