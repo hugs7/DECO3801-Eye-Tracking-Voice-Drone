c4f8f0b1f91c9ada7c30e263339b940766035f39
Hugo Burton <burto732@gmail.com>
2024-08-30 19:34:16 +1000

    Start drone display stream

--- drone/tello_drone/connect.py
+++ drone/tello_drone/connect.py
@@ -8,10 +8,16 @@ import KeyboardTelloModule as kp
 
 def tello_connect() -> tello.Tello:
     # Start Connection With Drone
-    Drone = tello.Tello()
-    Drone.connect()
+    drone = tello.Tello()
+    drone.connect()
 
-    return Drone
+    # Start Camera Display Stream
+    drone.streamon()
+    drone.set_speed(100)
+
+    print("Drone battery:", drone.get_battery())
+
+    return drone
 
 
 def init():
