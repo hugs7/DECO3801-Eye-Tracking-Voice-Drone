78b299fd360e82b6e527dcc2347b3ba6fe92da09
Hugo Burton <burto732@gmail.com>
2024-08-30 20:15:02 +1000

    Implement TelloDrone class

---
+++ drone/tello/tello_drone.py
@@ -0,0 +1,35 @@
+"""
+Defines class for Tello drone
+"""
+
+from ..drone import Drone
+from djitellopy import tello
+import cv2
+
+import constants as c
+
+
+class TelloDrone(Drone):
+    def __init__(self) -> None:
+        tello_drone = tello.Tello()
+        tello_drone.connect()
+
+        # Start Camera Display Stream
+        tello_drone.streamon()
+        tello_drone.set_speed(c.TELLO_SPEED_CM_S)
+
+        self.drone = tello_drone
+
+        print("Drone battery:", tello_drone.get_battery())
+
+        return tello_drone
+
+    def read_camera(self) -> cv2.typing.MatLike:
+        """
+        Reads the camera feed from the drone
+        :return: img - the image from the camera feed
+        """
+
+        frame_read = self.drone.get_frame_read()
+        img = frame_read.frame
+        return img
