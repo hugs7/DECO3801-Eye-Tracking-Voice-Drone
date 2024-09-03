d0796f6a6d50a7ea2cb5490d469be26783d46358
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-09-03 11:24:59 +1000

    Configure Model View Controller instance in basic class format

---
+++ drone/MainGui.py
@@ -0,0 +1,22 @@
+from ControllerGui import Controller
+from TelloModel import TelloModel
+from Gui import DroneApp
+import tkinter as tk
+
+
+def init(drone_type):
+    if drone_type == "mavic":
+        pass
+    else:
+        return TelloModel()
+
+def main():
+    drone_type = "tello"
+    drone = init(drone_type)
+    controller = Controller(drone)
+
+    root = tk.Tk()
+    app = DroneApp(root, controller)
+    root.mainloop()
+
+
