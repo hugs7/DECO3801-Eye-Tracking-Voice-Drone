8d01cb8abebfc92fb2655aa472b232ef7612551d
Hugo Burton <burto732@gmail.com>
2024-09-06 21:03:32 +1000

    Handle thread interrupt

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ Hugo Burton
 06/09/2024
 """
 
-from threading import Thread
+from threading import Thread, Event
 import logging
+from time import sleep
 
 if __name__ == "__main__":
     from eye_tracking import main as eye_tracking_loop
@@ -16,11 +17,27 @@ else:
     from .voice_control import main as voice_control_loop
     from .drone import main as drone_loop
 
+stop_event = Event()
+
+
+def thread_wrapper(func) -> None:
+    """
+    Wrapper function to run a function in a thread.
+    Passes a stop event to the function as a signal to stop execution.
+    :param func: Function to run in a thread
+    :return: None
+    """
+    while not stop_event.is_set():
+        func(stop_event)
+
+
+def is_any_thread_alive(threads):
+    return any(t.is_alive() for t in threads)
+
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info(" >>> Begin")
-    print("Welcome to mock drone controller!")
 
     # Create threads for each of the components
     threads = [
@@ -29,11 +46,22 @@ def main():
         Thread(target=lambda: thread_wrapper(drone_loop)),
     ]
 
+    # Start all threads
     for thread in threads:
         thread.start()
 
-    for thread in threads:
-        thread.join()
+    try:
+        # Periodically check if any thread is alive
+        while is_any_thread_alive(threads):
+            # Sleep for a short duration to prevent busy-waiting
+            sleep(0.1)
+    except KeyboardInterrupt:
+        logging.info("Interrupted! Stopping all threads...")
+        stop_event.set()  # Signal all threads to stop
+
+        # Ensure all threads are properly joined after signaling them to stop
+        for thread in threads:
+            thread.join()
 
     logging.info(" <<< End")
 
