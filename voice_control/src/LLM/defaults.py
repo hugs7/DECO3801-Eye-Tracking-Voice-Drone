83d4e1aab396d7bfddd8cf8e2d404b4855d45eb4
Hugo Burton <burto732@gmail.com>
2024-09-14 09:54:52 +1000

    Add doc strings

--- voice_control/src/LLM/defaults.py
+++ voice_control/src/LLM/defaults.py
@@ -1,3 +1,7 @@
+"""
+This module contains the default values for the LLM model.
+"""
+
 import json
 import os
 import ast
@@ -5,6 +9,15 @@ from file_handler import get_data_folder, get_context_file
 
 
 def init_context() -> list[dict]:
+    """
+    Initializes the context for the LLM model.
+
+    This function reads the initial context from the 'context.jsonl' file in the 'data' folder.
+
+    Returns:
+        list[dict]: The initial context as a list of dictionaries
+    """
+
     data_folder = get_data_folder()
     context_path = get_context_file()
     initial_path = os.path.join(data_folder, "initial.jsonl")
