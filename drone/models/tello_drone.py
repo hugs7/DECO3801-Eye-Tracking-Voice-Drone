aaf602802e5d18e82d17b9de2b42adc1748231d5
Hayden Reynolds <haydreynolds03@gmail.com>
2024-09-13 10:54:09 +1000

    added override commands for the tello drone control. sends data without polling

--- drone/models/tello_drone.py
+++ drone/models/tello_drone.py
@@ -26,6 +26,8 @@ class TelloDrone(Drone):
         pygame.init()
         self.window = pygame.display.set_mode((400, 400))
 
+        self.polling_flag = False
+
     def __connect(self) -> None:
         """
         Connects to the drone
@@ -42,3 +44,111 @@ class TelloDrone(Drone):
         frame_read = self.drone.get_frame_read()
         img = frame_read.frame
         return img
+
+
+    def set_video_resolution(self, resolution: str):
+        """Sets the resolution of the video stream
+        Use one of the following for the resolution argument:
+            Tello.RESOLUTION_480P
+            Tello.RESOLUTION_720P
+        """
+        cmd = 'setresolution {}'.format(resolution)
+        self.drone.send_control_command(cmd)
+
+    def set_video_fps(self, fps: str):
+        """Sets the frames per second of the video stream
+        Use one of the following for the fps argument:
+            Tello.FPS_5
+            Tello.FPS_15
+            Tello.FPS_30
+        """
+        cmd = 'setfps {}'.format(fps)
+        self.drone.send_control_command(cmd)
+
+    def send_info(self, command):
+        self.drone.send_command_without_return(command)
+        #data, address = client_socket.recvfrom(1024)
+        #client_socket.sendto(command.encode('utf-8'), address)
+
+
+    # Controlling methods
+    '''def send_control_command(self, command: str, timeout: int = RESPONSE_TIMEOUT) -> bool:
+        """Send control command to Tello and wait for its response.
+        Internal method, you normally wouldn't call this yourself.
+        """
+        response = "max retries exceeded"
+        for i in range(0, self.retry_count):
+            response = self.send_command_without_return(command, timeout=timeout) #USUALLY SEND WITH RETURN
+
+            if 'ok' in response.lower():
+                return True
+
+            self.LOGGER.debug("Command attempt #{} failed for command: '{}'".format(i, command))
+
+        self.raise_result_error(command, response)
+        return False # never reached
+    '''
+
+    #valid tello command strings:
+    '''
+    Connect = "command"
+    Takeoff = "takeoff", 20sec
+    Land = "land"
+    Stream on = "streamon"
+    Stream off = "streamoff"
+    Emergency off = "emergency"
+    Movement = "direction magnitude"
+    directions = {"up", "down", "left", "right", "forward", "back"}
+    Rotate CW = "cw magnitude"
+    Rotate CCW = "ccw magnitude"
+    Flip = "flip direction"
+    directions = {"l", "r", "f", "b"}
+    '''
+
+    def rotate_clockwise(self, degrees: int) -> None:
+        command = "cw {}".format(degrees)
+        self.drone.send_info(command)
+
+    def rotate_counter_clockwise(self, degrees: int) -> None:
+        command = "ccw {}".format(degrees)
+        self.drone.send_info(command)
+
+    def move_up(self, cm: int) -> None:
+        command = "up {}".format(cm)
+        self.drone.send_info(command)
+
+    def move_down(self, cm: int) -> None:
+        command = "down {}".format(cm)
+        self.drone.send_info(command)
+
+    def move_left(self, cm: int) -> None:
+        command = "left {}".format(cm)
+        self.dronef.send_info(command)
+
+    def move_right(self, cm: int) -> None:
+        command = "right {}".format(cm)
+        self.drone.send_info(command)
+
+    def move_forward(self, cm: int) -> None:
+        command = "forward {}".format(cm)
+        self.drone.send_info(command)
+
+    def move_backward(self, cm: int) -> None:
+        command = "back {}".format(cm)
+        self.drone.send_info(command)
+
+    def takeoff(self) -> None:
+        command = "takeoff"
+        self.drone.send_info(command)
+
+    def land(self) -> None:
+        command = "land"
+        self.drone.send_info(command)
+
+    # Polling methods
+    def get_altitude(self) -> int:
+        """
+        Gets the altitude of the drone
+        :return: The altitude of the drone
+        """
+        pass
