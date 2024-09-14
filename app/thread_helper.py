5a17f02df240280078b5fd0ab60fc4e92ee56859
Hugo Burton <burto732@gmail.com>
2024-09-14 17:00:23 +1000

    Exit parent thread of quit gaze detector

--- app/thread_helper.py
+++ app/thread_helper.py
@@ -41,6 +41,28 @@ def thread_loop_handler(stop_event: Optional[Event]) -> None:
         raise SystemExit
 
 
+def thread_exit(stop_event: Optional[Event]) -> None:
+    """
+    Helper function to handle stop event in threads.
+
+    Args:
+        stop_event: Event to signal stop
+
+    Returns:
+        None
+    """
+
+    if stop_event is None:
+        # Module is running independently (main thread)
+        return
+
+    # Module is running as a child thread
+    if stop_event.is_set():
+        thread = current_thread()
+        logger.critical(f"Received stop signal from '{thread.name}'. Exiting thread...")
+        raise SystemExit
+
+
 def get_function_name(func) -> str:
     """
     Inspects the func to get the function's name
