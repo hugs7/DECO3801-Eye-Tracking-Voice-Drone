54a8d8aaca329d97214d0fe07c40b53fc9559f7b
Hugo Burton <burto732@gmail.com>
2024-09-14 15:45:19 +1000

    Fix object attr access in shared data

--- app/eye_tracking.py
+++ app/eye_tracking.py
@@ -28,7 +28,7 @@ def loop(shared_data: Optional[OmegaConf] = None, data_lock: Optional[Lock] = No
     logger.info(f"Set random number: {random_num}")
     if not is_main_thread():
         with data_lock:
-            shared_data.eye_tracking_data = random_num
+            shared_data.eye_tracking = random_num
 
     logger.debug("<<< End Eye Tracking loop")
 
