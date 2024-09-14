49202258dc3445d9ee4e954de0065daddc27ff23
Hugo Burton <burto732@gmail.com>
2024-09-14 10:59:24 +1000

    Fix timestamp in audio save path

--- voice_control/src/date.py
+++ voice_control/src/date.py
@@ -23,3 +23,13 @@ def timestamp(format="%Y-%m-%d %H:%M:%S") -> str:
     logger.info(f"Timestamp: {timestamp}")
 
     return timestamp
+
+
+def timestamp_filename_safe() -> str:
+    """
+    Returns the current timestamp in a filename-safe format 'YYYY-MM-DD_HH-MM-SS'.
+
+    Returns:
+        str: The current timestamp.
+    """
+    return timestamp("%Y-%m-%d_%H-%M-%S")
