3d78e42a9693e3e0ecefe8351b866cf03df99f84
Hugo Burton <burto732@gmail.com>
2024-09-14 15:16:00 +1000

    Import eye tracking and voice control real modules

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ import importlib
 import logging
 from threading import Thread, Event, Lock
 from time import sleep
+import os
+import sys
 
 import constants as c
 from thread_helper import get_function_module
@@ -16,29 +18,32 @@ from logger_helper import init_root_logger, disable_logger
 root_logger = init_root_logger()
 
 
-def dynamic_import(module_name: str, alias: str):
+def dynamic_import(module_path: str, alias: str):
     """
-    Dynamically imports a module based on the current script context.
+    Dynamically imports a module based on the given path.
 
     Args:
-        module_name: Name of the module to import
-        alias: Alias for the imported module
+        module_path: Relative path to the module to import
+        alias: Alias for the imported module's main function
 
     Returns:
-        Imported module
+        Imported module's function
     """
-    if __name__ == "__main__":
-        module = importlib.import_module(module_name)
-    else:
-        # Handle relative imports
-        module = importlib.import_module(
-            f".{module_name}", package=__package__)
+    # Go up one directory from app/main.py to project root
+    project_root = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), ".."))
+
+    # Adjust the system path to include project root
+    sys.path.insert(0, project_root)
+
+    # Dynamically import the module
+    module = importlib.import_module(module_path)
 
     return getattr(module, alias)
 
 
-eye_tracking = dynamic_import("eye_tracking", "main")
-voice_control = dynamic_import("voice_control", "main")
+eye_tracking = dynamic_import("eye_tracking.src.main", "main")
+voice_control = dynamic_import("voice_control.src.main", "main")
 drone = dynamic_import("drone", "main")
 
 # === Globals ===
