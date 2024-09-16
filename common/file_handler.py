eded8a68b41dcb418ff3d49fce25a831b32f070a
Hugo Burton <burto732@gmail.com>
2024-09-16 19:32:04 +1000

    Simplify import of Path

--- common/file_handler.py
+++ common/file_handler.py
@@ -2,23 +2,22 @@
 Common file handler functions
 """
 
-import os
-import pathlib
+from pathlib import Path
 
 
-def get_project_root() -> pathlib.Path:
+def get_project_root() -> Path:
     """
     Returns the root folder of the project, assuming 'eye_tracking' is part of the directory structure.
 
     Returns:
-        pathlib.Path: The path to the root folder of the project.
+        Path: The path to the root folder of the project.
     """
 
-    project_root = pathlib.Path(__file__).parent.parent.resolve()
+    project_root = Path(__file__).parent.parent.resolve()
     return project_root
 
 
-def get_common_folder() -> pathlib.Path:
+def get_common_folder() -> Path:
     """
     Returns the path to the 'common' folder.
 
