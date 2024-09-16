e8e327e8c6447eaa04d04f726d160c83c77c9755
Hugo Burton <burto732@gmail.com>
2024-09-17 00:21:47 +1000

    Add label with alignment function

--- app/src/common_gui.py
+++ app/src/common_gui.py
@@ -3,11 +3,16 @@ Defines common methods for the GUI
 """
 
 from PyQt5.QtWidgets import QMenu, QAction, QPushButton, QLabel
+from PyQt5.QtCore import Qt
 
 
 class CommonGUI:
     def __init__(self):
         self.layout = None
+
+    def __check_layout(self):
+        if self.layout is None:
+            raise ValueError("Layout must be set before adding a widget")
 
     def _add_menu_action(self, menu: QMenu, action_name: str, callback: callable) -> None:
         """
@@ -37,8 +42,7 @@ class CommonGUI:
         Returns:
             None
         """
-        if self.layout is None:
-            raise ValueError("Layout must be set before adding a button")
+        self.__check_layout()
 
         button = QPushButton(label)
         button.clicked.connect(callback)
@@ -53,3 +57,21 @@ class CommonGUI:
         """
         label = QLabel(text)
         self.layout.addWidget(label)
+
+    def _add_label_with_alignment(self, alignment: Qt.Alignment | Qt.AlignmentFlag) -> QLabel:
+        """
+        Add a label to the preferences dialog
+
+        Args:
+            text: Label text
+
+        Returns:
+            The label widget
+        """
+        self.__check_layout()
+
+        label = QLabel(self)
+        label.setAlignment(alignment)
+        self.layout.addWidget(label)
+
+        return label
