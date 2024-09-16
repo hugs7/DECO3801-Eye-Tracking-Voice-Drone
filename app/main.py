75b4a27961c8317fc612c6fba34d9c41fb4d807d
Hugo Burton <burto732@gmail.com>
2024-09-16 20:03:43 +1000

    Only perform imports in __main__ not __mp_main__

--- app/main.py
+++ app/main.py
@@ -20,13 +20,16 @@ from conf_helper import safe_get
 
 logger = init_logger()
 
-logger.info(">>> Begin")
+if __name__ == "__main__":
+    logger.info(">>> Begin")
 
-logger.info("Initialising modules...")
-eye_tracking = dynamic_import("eye_tracking.src.main", "main")
-voice_control = dynamic_import("voice_control.src.main", "main")
-drone = dynamic_import("drone", "main")
-logger.info("Modules initialised.")
+    logger.info("Initialising modules...")
+    eye_tracking = dynamic_import("eye_tracking.src.main", "main")
+    voice_control = dynamic_import("voice_control.src.main", "main")
+    drone = dynamic_import("drone", "main")
+    logger.info("Modules initialised.")
+elif __name__ == "__mp_main__":
+    logger.info(">>> Begin Multiprocessing")
 
 
 def is_any_thread_alive(threads: List[Thread]):
