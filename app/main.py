2fad8b90095b61e7e6e1addbf2f890bb4ea88ce3
Hugo Burton <burto732@gmail.com>
2024-09-16 18:55:56 +1000

    Migrate voice process to be handled in main

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ Hugo Burton
 """
 
 from threading import Thread, Event, Lock
-from time import sleep
+from multiprocessing import Manager, Process
 from typing import List
 
 from omegaconf import OmegaConf
@@ -61,31 +61,45 @@ def main():
     logger.info(">>> Main Begin")
 
     # Create threads for each of the components
-    thread_functions = [eye_tracking, voice_control, drone]
+    thread_functions = [eye_tracking, drone]
     shared_data = OmegaConf.create({get_function_module(func): OmegaConf.create() for func in thread_functions})
     threads = [
         Thread(target=lambda func=func: func(stop_event, shared_data, data_lock), name=f"thread_{get_function_module(func)}")
         for func in thread_functions
     ]
 
-    # Start all threads
-    for thread in threads:
-        thread.start()
-
+    # Voice Control is process
     try:
-        # Periodically check if any thread is alive
-        while is_any_thread_alive(threads):
-            main_loop(shared_data)
+        logger.info("Initialising voice process")
+        manager = Manager()
+        manager_data = manager.dict()
+        process_functions = [voice_control]
+        process_shared_dict = {get_function_module(func): None for func in process_functions}
+        manager_data.update(process_shared_dict)
+        processes = [Process(target=func, args=(manager_data,), name=f"process_{get_function_module(func)}") for func in process_functions]
+        for process in processes:
+            process.start()
 
-            # Sleep for a short duration to prevent busy-waiting
-            sleep(c.BUSY_WAIT_PERIOD_SECONDS)
+        # Start all threads
+        logger.info("Initialising threads")
+        for thread in threads:
+            thread.start()
     except KeyboardInterrupt:
         logger.critical("Interrupted! Stopping all threads...")
         stop_event.set()
 
-        # Ensure all threads are properly joined after signaling them to stop
-        for thread in threads:
-            thread.join()
+    logger.info("Signalling all threads to stop")
+    stop_event.set()
+
+    # Ensure all threads and processes are properly joined after signaling them to stop
+    for process in processes:
+        process.join()
+        if process.is_alive():
+            logger.info(f"Terminating process {process.name}")
+            process.terminate()
+
+    for thread in threads:
+        thread.join()
 
     logger.debug("<<< End")
 
