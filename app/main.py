f058d0b7fcbe94bdbc1b66538f11715215ef47c1
Hugo Burton <burto732@gmail.com>
2024-09-17 21:28:47 +1000

    Add comments to main to explain reasoning for process and thread usage
    + explained shared data handling

--- app/main.py
+++ app/main.py
@@ -28,13 +28,11 @@ voice_control = dynamic_import("voice_control.src.main", "main")
 drone = dynamic_import("drone", "main")
 logger.info("Modules initialised.")
 
-# === Globals ===
-
-stop_event = Event()
-data_lock = Lock()
-
 
 def is_any_thread_alive(threads: List[Thread]):
+    """
+    Check if any thread is alive.
+    """
     return any(t.is_alive() for t in threads)
 
 
@@ -60,16 +58,17 @@ def main_loop(shared_data: OmegaConf):
 def main():
     logger.info(">>> Main Begin")
 
-    # Create threads for each of the components
-    thread_functions = [eye_tracking, drone]
-    shared_data = OmegaConf.create({get_function_module(func): OmegaConf.create() for func in thread_functions})
-    threads = [
-        Thread(target=lambda func=func: func(stop_event, shared_data, data_lock), name=f"thread_{get_function_module(func)}")
-        for func in thread_functions
-    ]
+    # Define lock and stop event early to ensure KeyboardInterrupt
+    # can signal all threads to stop.
+    stop_event = Event()
+    data_lock = Lock()
 
-    # Voice Control is process
     try:
+        # =========== Processes ===========
+        # Due to blocking operation, the voice control module is run in a Process
+        # (instead of a Thread) to allow for parallel execution and termination on
+        # parent process exit. As a result, the shared data is managed by a Manager
+        # object to allow for inter-process communication.
         logger.info("Initialising voice process")
         manager = Manager()
         manager_data = manager.dict()
@@ -78,12 +77,22 @@ def main():
         manager_data.update(process_shared_dict)
         processes = [Process(target=func, args=(manager_data,), name=f"process_{get_function_module(func)}") for func in process_functions]
 
-        logger.info("Initialising drone process")
+        logger.info("Initialising processes")
         for process in processes:
             logger.debug(f"Starting process {process.name}")
             process.start()
 
-        # Start all threads
+        # =========== Threads ===========
+
+        # The remaining components (eye tracking and drone) are run in threads.
+        # Threads use a different shared_data object because threads share memory.
+        # but also require a lock to prevent race conditions when accessing shared data.
+        thread_functions = [eye_tracking, drone]
+        shared_data = OmegaConf.create({get_function_module(func): OmegaConf.create() for func in thread_functions})
+        threads = [
+            Thread(target=lambda func=func: func(stop_event, shared_data, data_lock), name=f"thread_{get_function_module(func)}")
+            for func in thread_functions
+        ]
         logger.info("Initialising threads")
         for thread in threads:
             logger.debug(f"Starting thread {thread.name}")
