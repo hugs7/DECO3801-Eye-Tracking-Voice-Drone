77ea3558aa2def36995a4eda4c56aff87cb584f1
Hugo Burton <burto732@gmail.com>
2024-09-20 19:51:02 +1000

    Add get attribute override for tello drone
    Remove unused methods

--- drone/src/models/tello_drone.py
+++ drone/src/models/tello_drone.py
@@ -2,6 +2,9 @@
 Defines class for Tello drone
 """
 
+from datetime import datetime
+from typing import Any
+
 import cv2
 from djitellopy import tello
 from omegaconf import OmegaConf
@@ -40,6 +43,8 @@ class TelloDrone(Drone):
         self.drone.streamon()
         self.drone.set_speed(c.TELLO_SPEED_CM_S)
 
+        self.last_command_time = datetime.now()
+
         logger.info("Drone battery: %d", self.drone.get_battery())
 
     def connect(self) -> None:
@@ -70,79 +75,19 @@ class TelloDrone(Drone):
 
         return img
 
-    def set_video_resolution(self, resolution: str):
-        """Sets the resolution of the video stream
-        Use one of the following for the resolution argument:
-            Tello.RESOLUTION_480P
-            Tello.RESOLUTION_720P
-        """
-        cmd = "setresolution {}".format(resolution)
-        self.drone.send_control_command(cmd)
-
-    def set_video_fps(self, fps: str):
-        """Sets the frames per second of the video stream
-        Use one of the following for the fps argument:
-            Tello.FPS_5
-            Tello.FPS_15
-            Tello.FPS_30
-        """
-        cmd = "setfps {}".format(fps)
-        self.drone.send_control_command(cmd)
-
-    def set_video_bitrate(self, bitrate: int):
-        """Sets the bitrate of the video stream
-        Use one of the following for the bitrate argument:
-            Tello.BITRATE_AUTO
-            Tello.BITRATE_1MBPS
-            Tello.BITRATE_2MBPS
-            Tello.BITRATE_3MBPS
-            Tello.BITRATE_4MBPS
-            Tello.BITRATE_5MBPS
-        """
-        cmd = "setbitrate {}".format(bitrate)
-        self.drone.send_control_command(cmd)
-
     def send_info(self, command):
+        """
+        Override for
+        """
+
         if self.polling_flag:
             self.drone.send_control_command(command)
         else:
+            if not self.__has_waited_between_commands():
+                logger.info("Wait before sending command")
+                return
+
             self.drone.send_command_without_return(command)
-        # data, address = client_socket.recvfrom(1024)
-        # client_socket.sendto(command.encode('utf-8'), address)
-
-    # Controlling methods
-    '''def send_control_command(self, command: str, timeout: int = RESPONSE_TIMEOUT) -> bool:
-        """Send control command to Tello and wait for its response.
-        Internal method, you normally wouldn't call this yourself.
-        """
-        response = "max retries exceeded"
-        for i in range(0, self.retry_count):
-            response = self.send_command_without_return(command, timeout=timeout) #USUALLY SEND WITH RETURN
-
-            if 'ok' in response.lower():
-                return True
-
-            self.LOGGER.debug("Command attempt #{} failed for command: '{}'".format(i, command))
-
-        self.raise_result_error(command, response)
-        return False # never reached
-    '''
-
-    # valid tello command strings:
-    """
-    Connect = "command"
-    Takeoff = "takeoff", 20sec
-    Land = "land"
-    Stream on = "streamon"
-    Stream off = "streamoff"
-    Emergency off = "emergency"
-    Movement = "direction magnitude"
-    directions = {"up", "down", "left", "right", "forward", "back"}
-    Rotate CW = "cw magnitude"
-    Rotate CCW = "ccw magnitude"
-    Flip = "flip direction"
-    directions = {"l", "r", "f", "b"}
-    """
 
     def rotate_clockwise(self, degrees: int) -> None:
         command = "cw {}".format(degrees)
@@ -188,10 +133,22 @@ class TelloDrone(Drone):
         command = "flip f"
         self.send_info(command)
 
-    # Polling methods
-    def get_altitude(self) -> int:
+    def __getattribute__(self, name: str) -> Any:
         """
-        Gets the altitude of the drone
-        :return: The altitude of the drone
+        Returns attributes from the drone object. Usefulf for drone polling methods.
+        E.g. drone.get_height()
         """
-        pass
+        if name in Drone.__dict__:
+            # Use local method if it exists
+            return super().__getattribute__(name)
+
+        # Look for attribute in drone object
+        return getattr(self.drone, name)
+
+    def __has_waited_between_commands(self) -> bool:
+        """
+        Checks if the time between commands is greater than the minimum time between commands
+        :return: True if the time between commands is greater than the minimum time between commands
+        """
+        time_diff = datetime.now() - self.last_command_time
+        return time_diff.total_seconds() >= self.drone.TIME_BTW_COMMANDS
