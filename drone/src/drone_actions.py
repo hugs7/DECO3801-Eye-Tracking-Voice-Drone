a6764b3dbdac65b470bb65f192946668fe1a2caf
Hugo Burton <burto732@gmail.com>
2024-09-20 23:32:30 +1000

    Define drone actions class

---
+++ drone/src/drone_actions.py
@@ -0,0 +1,19 @@
+"""
+Drone actions definition
+"""
+
+
+class DroneActions:
+    TAKEOFF = "takeoff"
+    LAND = "land"
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    RIGHT = "right"
+    FORWARD = "forward"
+    BACKWARD = "backward"
+    ROTATE_CW = "cw"
+    ROTATE_CCW = "ccw"
+    FLIP_FORWARD = "flip forward"
+    SPEED = "speed"
+    EMERGENCY = "emergency"
