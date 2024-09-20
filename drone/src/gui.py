48b7dd4bf85640bcc02386622c1609f8c159fbd2
Hugo Burton <burto732@gmail.com>
2024-09-20 14:35:29 +1000

    Code formatting

--- drone/src/gui.py
+++ drone/src/gui.py
@@ -24,17 +24,15 @@ class DroneApp:
         loading_screen_path = fh.get_assets_folder() / "loadingScreen.png"
         base_image = Image.open(loading_screen_path)
         # Resize the image to fit the canvas
-        base_image = base_image.resize(
-            (self.WIDTH, self.HEIGHT), Image.Resampling.BILINEAR)
+        base_image = base_image.resize((self.WIDTH, self.HEIGHT), Image.Resampling.BILINEAR)
         base_image = ImageTk.PhotoImage(base_image)
 
         # Display the base image first
-        self.image_on_canvas = self.canvas.create_image(
-            0, 0, anchor=tk.NW, image=base_image)
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tk.NW, image=base_image)
 
         self.update_video_feed()
 
-        self.root.bind('<KeyPress>', self.on_key_press)
+        self.root.bind("<KeyPress>", self.on_key_press)
 
     def on_key_press(self, event):
         self.controller.handle_input(event.keysym)
