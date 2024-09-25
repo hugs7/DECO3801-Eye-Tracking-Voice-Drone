1a6b0f8ec3d665b23713224c2e101f4eb1627605
Hugo Burton <burto732@gmail.com>
2024-09-25 20:37:27 +1000

    Separate GUI designer init from GUI logic

--- ui_starter/run.py
+++ ui_starter/run.py
@@ -1,9 +1,23 @@
-from designer import QMainWindow
-from PyQt6.QtWidgets import QApplication
+"""
+Run basic gui
+"""
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 
-app = QApplication(sys.argv)
-app.setStyle("Fusion")
-window = QMainWindow()
-window.show()
-sys.exit(app.exec())
+
+from designer import Ui_MainWindow
+
+
+class App(QApplication, Ui_MainWindow):
+
+    def __init__(self):
+        app = QApplication(sys.argv)
+        app.setStyle("Fusion")
+        self.main_window = QMainWindow()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.main_window.show()
+    sys.exit(app.exec())
