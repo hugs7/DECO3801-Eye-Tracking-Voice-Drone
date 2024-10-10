e394093cf28e49b3de78c542ca6e6aabe6c353ca
Jas2310 <jasmine.hong@uqconnect.edu.au>
2024-10-11 09:56:42 +1000

    Remove gui signals

--- app/src/gui_signals.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtCore import pyqtSignal, QObject
- 
-
-class GuiSignals(QObject):
-    updateCommand = pyqtSignal(str)
-    resize = pyqtSignal()
-    
