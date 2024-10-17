9bae9f42a159c5474c4f724c273d86deb1e37b5e
Hugo Burton <burto732@gmail.com>
2024-10-17 23:00:02 +1000

    Lower log level of statistics

--- app/src/gui.py
+++ app/src/gui.py
@@ -471,7 +471,7 @@ class MainApp(QMainWindow, CommonGUI):
             return
 
         flight_statistics: Dict = drone_data[cc.FLIGHT_STATISTICS]
-        logger.debug("Flight statistics: %s", flight_statistics)
+        logger.trace("Flight statistics: %s", flight_statistics)
         if not flight_statistics:
             return None
 
