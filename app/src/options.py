c8d584cb4a9067cd73f80757aef972a37d7b1d60
Hugo Burton <burto732@gmail.com>
2024-10-25 14:54:22 +1000

    Fix buttons in options

--- app/src/options.py
+++ app/src/options.py
@@ -2,7 +2,7 @@
 Defines the options window for the gui
 """
 
-
+from PyQt6.QtWidgets import QVBoxLayout
 from PyQt6.QtCore import QMetaObject
 
 from common.common_dialog import CommonDialog
@@ -10,14 +10,18 @@ from common.common_dialog import CommonDialog
 
 class PreferencesDialog(CommonDialog):
     def __init__(self):
-        super().__init__("Drone Control Preferences", 300, 300)
+        super().__init__("Drone Control Preferences", 500, 350)
 
         self.__init_ui()
 
     def __init_ui(self):
+        # Set window properties
         self.setObjectName("Options")
-        self.resize(389, 300)
 
+        # Create layout
+        self.layout = QVBoxLayout(self)
+
+        # Create and add buttons to layout
         self.ok_button = self._add_button("OK", self.accept)
         self.cancel_button = self._add_button("Cancel", self.reject)
 
