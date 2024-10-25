ab4ec8edeea596e42283f1a21d804e3056be88a9
Hugo Burton <burto732@gmail.com>
2024-10-25 15:26:56 +1000

    Exit audio loop if network not available

--- common/network.py
+++ common/network.py
@@ -5,8 +5,26 @@ Common network functions
 
 import socket
 
+from .logger_helper import init_logger
+
+logger = init_logger()
+
 
 def check_internet_connection(host="www.google.com.au", port=80, timeout=5):
+    """
+    Check if the device is connected to the internet.
+
+    Args:
+        host (str, optional): The host to check for connection. Defaults to "www.google.com.au".
+        port (int, optional): The port to check for connection. Defaults to 80.
+        timeout (int, optional): The timeout for the connection check. Defaults to 5.
+
+    Returns:
+        bool: True if the device is connected to the internet, False otherwise.
+    """
+
+    logger.info("Checking internet connection at %s:%s", host, port)
+
     try:
         # Attempt to resolve the host to check for connection
         socket.setdefaulttimeout(timeout)
